98ec8f76a37e9ec62ba309f0611c984c69e6b94e
ebenzOW <ebenz@oldwestbury.edu>
2025-10-27 18:12:34 -0400

    Coding

--- Bitmap/Bitmap.cpp
+++ Bitmap/Bitmap.cpp
@@ -1,7 +1,34 @@
 #include <iostream>
+#include <deque>
+
+#include "bmp.hpp"
+
+using namespace std;
+struct pixel {
+    int x;
+    int y;
+    pixel(int x_ = 0, int y_ = 0 ) : x{ x_ }, y{ y_ } {}
+};
+
+void floodFill(BMP image, int startX, int startY, color fillColor)
+{
+    int h = image.get_height();
+    int w = image.get_width();
+    if (startX < 0 || startX > w || startY <0 || startY > h)
+        return;
+
+    color startColor = image.get_pixel(startX, startY); //Getting the original color of the space
+
+    deque<pixel> fillDeque;
+    fillDeque.push_back(pixel(startX, startY));
+
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+
+    BMP image("sample2.bmp");
+    image.write("img1.bmp");
+  
 }
 
