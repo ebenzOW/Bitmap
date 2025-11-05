0450913f6d9680aab5c56c929999ae31358c1712
ebenzOW <ebenz@oldwestbury.edu>
2025-11-05 15:19:28 -0500

    floodfill fixed

--- Bitmap/Bitmap.cpp
+++ Bitmap/Bitmap.cpp
@@ -13,11 +13,11 @@ struct pixel {
     pixel(int x_ = 0, int y_ = 0 ) : x{ x_ }, y{ y_ } {}
 };
 
-void floodFill(BMP image, int startX, int startY, color fillColor)
+void floodFill(BMP &image, int startX, int startY, color fillColor)
 {
     int h = image.get_height();
     int w = image.get_width();
-    if (startX < 0 || startX > w || startY <0 || startY > h)
+    if (startX < 0 || startX >= w || startY <0 || startY >= h)
         return;
 
     color startColor = image.get_pixel(startX, startY); //Getting the original color of the space
@@ -38,18 +38,19 @@ void floodFill(BMP image, int startX, int startY, color fillColor)
         if (tempColor.r == startColor.r && tempColor.b == startColor.b
             && tempColor.g == startColor.g) 
         {
-            int x = temp.x;
-            int y = temp.y;
             image.set_pixel(temp.x, temp.y, fillColor);
-            for(x = x-1; x<=temp.x+1; x++) //find the neighbors
-                for (y = y - 1; y <= temp.y + 1; y++)
+            for (int x = temp.x - 1; x <= temp.x + 1; x++) //find the neighbors
+            {
+                for (int y = temp.y - 1; y <= temp.y + 1; y++)
                 {
-                    if ((x >=0 && x <= w && y >=0 && y <= h) && (visited[x][y] == false))
+                    if (x == temp.x && y == temp.y) continue;
+                    if ((x >= 0 && x < w && y >= 0 && y < h) && (visited[x][y] == false))
                     {
                         fillDeque.push_back(pixel(x, y));
                         visited[x][y] = true;
                     }
                 }
+            }
         }
        
     }
@@ -61,7 +62,7 @@ int main()
     BMP image("sample2.bmp");
     color fill(255, 0, 0);
     floodFill(image, 300, 300, fill);
-    image.write("C:\\Users\\ebenz\\Downloads\\img1.bmp");
+    image.write("C:\\Users\\erics\\Documents\\GitHub\\Bitmap\\Bitmap\\image2.bmp");
   
 }
 
